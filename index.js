e6d724dcc41f8ee178dc22de581988843aeef455
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-03-05 17:44:05 +0000

    set up

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // implement your API here
+//Functions in file: find, findById, insert, update, remove
+
+const express = require("express")
+const server = express()
+server.use(express.json());
+
+const db = require('./data/db');
+
+
+//Add first data: function find --> gets all users,  assign variable
+
+server.get('/', (req, res) => {
+    res.json({message: 'test'})
+
+})
+
+
+const port = 8000
+
+server.listen(port, () => {
+    console.log(`Server stated at ${port}`)
+})
