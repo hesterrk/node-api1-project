9e9fc9594e739b402327f54d148c227cbcff3814
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-03-05 18:02:39 +0000

    completed get request all

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 // implement your API here
 //Functions in file: find, findById, insert, update, remove
 
-const express = require("express")
-const server = express()
+const express = require("express");
+const server = express();
 server.use(express.json());
 
-const db = require('./data/db');
+const db = require("./data/db");
+
+//GET: add first data: .find()--> gets all users
+
+server.get("/api/users", (req, res) => {
+
+  db.find()
+    .then(d => {
+      res.status(200).json(d);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ errorMessage: "The users information could not be retrieved."});
+    });
+});
 
 
-//Add first data: function find --> gets all users,  assign variable
-
-server.get('/', (req, res) => {
-    res.json({message: 'test'})
-
-})
 
 
-const port = 8000
+
+
+
+const port = 8000;
 
 server.listen(port, () => {
-    console.log(`Server stated at ${port}`)
-})
+  console.log(`Server stated at ${port}`);
+});
